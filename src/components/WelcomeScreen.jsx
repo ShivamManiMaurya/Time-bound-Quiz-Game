fc1f29d1747b95a5ff6dc3e18ee0509f30bf6153
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-05-13 16:40:49 +0530

    initial commit

---
+++ src/components/WelcomeScreen.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import WelcomeScreenRules from "./WelcomeScreenRules";
+
+function WelcomeScreen() {
+    const [viewRules, setViewRules] = useState(false);
+
+    const handleRules = () => {
+        setViewRules(true);
+    };
+
+    return (
+        <div className="welcome">
+            <div className="welcome-main-div">
+                <h1>Welcome to the Superhero Quiz Game!</h1>
+                <h3>
+                    Are you ready to test your knowledge of superheroes, comic
+                    books, and movies?
+                </h3>
+                <button onClick={handleRules}>Take Quiz</button>
+            </div>
+            {viewRules ? <WelcomeScreenRules /> : null}
+        </div>
+    );
+}
+
+export default WelcomeScreen;
---
+++ src/components/WelcomeScreenRules.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function WelcomeScreenRules() {
+    return (
+        <div className="welcome-rules-div">
+            <h1>Quiz Rules</h1>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h4>10 Mins</h4>
+                    <p>Keep in mind that it's a time-bound quiz</p>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h4>10 Questions</h4>
+                    <p>Let's test your superheroes knowledge</p>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h4>60% To be a Superhero</h4>
+                    <p>
+                        All the best! See you on the other side of the portal.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <button>Start</button>
+            </div>
+        </div>
+    );
+}
+
+export default WelcomeScreenRules;
