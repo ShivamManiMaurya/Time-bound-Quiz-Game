b0bd1efd6c9f6e0d75a1d54632e9757d539f2aa8
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-05-14 15:06:45 +0530

    style - welcome screen rules style done

--- src/components/WelcomeScreenRules.jsx
+++ src/components/WelcomeScreenRules.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "../components/styles/Welcome.css";
 import QuestionsScreen from "./QuestionsScreen";
+import TimerOutlinedIcon from "@mui/icons-material/TimerOutlined";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 function WelcomeScreenRules() {
     const [isVisible, setIsVisible] = useState(false);
@@ -12,23 +15,29 @@ function WelcomeScreenRules() {
     return (
         <div className="welcome-rules-div">
             <h1>Quiz Rules</h1>
-            <div>
-                <img src="" alt="" />
-                <div>
+            <div className="rules-div">
+                <div className="rules-mat-div">
+                    <TimerOutlinedIcon sx={{ color: "white", fontSize: 40 }} />
+                </div>
+                <div className="rules-text-div">
                     <h4>10 Mins</h4>
                     <p>Keep in mind that it's a time-bound quiz</p>
                 </div>
             </div>
-            <div>
-                <img src="" alt="" />
-                <div>
+            <div className="rules-div">
+                <div className="rules-mat-div">
+                    <QuestionAnswerIcon sx={{ color: "white", fontSize: 40 }} />
+                </div>
+                <div className="rules-text-div">
                     <h4>10 Questions</h4>
                     <p>Let's test your superheroes knowledge</p>
                 </div>
             </div>
-            <div>
-                <img src="" alt="" />
-                <div>
+            <div className="rules-div">
+                <div className="rules-mat-div">
+                    <EmojiEventsIcon sx={{ color: "white", fontSize: 40 }} />
+                </div>
+                <div className="rules-text-div">
                     <h4>60% To be a Superhero</h4>
                     <p>
                         All the best! See you on the other side of the portal.
@@ -36,7 +45,12 @@ function WelcomeScreenRules() {
                 </div>
             </div>
             <div>
-                <button onClick={handleVisibility}>Start</button>
+                <button
+                    className="welcome-take-quiz-btn rules-start-btn"
+                    onClick={handleVisibility}
+                >
+                    Start
+                </button>
             </div>
             {isVisible ? <QuestionsScreen /> : null}
         </div>
